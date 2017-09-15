7da579f2e6f977acea2bfc941c110f97dfeeaff4
Keith Getty <keithgetty@yahoo.com>
2017-09-15 02:04:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function deleteFromObjectByKey(object, key) {
   return newObject;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key);
+function destructivelyDeleteFromObjectByKey(object, key){
 delete object[key];
 return key;
+}
